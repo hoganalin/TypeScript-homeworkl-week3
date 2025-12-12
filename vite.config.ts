c37ceb0aa64ce11732f2496077930d558953bf7a
hoganalin <hoganalin@gmail.com>
2025-12-12 09:08:49 +0800

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
+  base: '/TypeScript-homeworkl-week3/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
